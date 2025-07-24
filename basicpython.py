16fec321a6d6fc1501eba0d230c18ba7f0394aad
root <root@ip-10-0-228-96.eu-north-1.compute.internal>
2025-07-24 10:43:24 +0000

    frist commit

---
+++ basicpython.py
@@ -0,0 +1,24 @@
+A = 10
+B = 20
+Name = 'vikas'
+Age  = 25
+height = 5.8
+
+
+print(Age)  ##it will print age of vikas , basically age is a integer.
+print(type(Age))  ## type is used to know which data type is . Output : <class 'int'>
+
+print (height)       ## 5.8
+print(type(height))  ## <class 'float'>
+
+
+
+         #Arithmatic operations
+
+print (A+B)      # 10 + 20 =30 addition
+print (A-B)      # 10 - 20 = -10 subtraction
+print (A*B)      # 10 * 20 = 200 multiplication
+print (A/B)      # 10 / 20 = 0.5 division
+print (A//B)     # 10 // 20 = 0 because it used to ignore the decimal. it will give absolute value.
+print (A**B)     # 10 ** 20  = 10000000000 .it is used to get power of value.10 to the power of 20 (10^20) is a very large number, equal to 1 followed by 20 zeros. It can be written as 100,000,000,000,000,000,000. 
+print (A % B)    # 10 % 20 
