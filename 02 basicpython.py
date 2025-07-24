1983ef0af996b362670fcadf6d06eed802f7b76f
Vikas-git <Vikasshetty543@gmail.com>
2025-07-24 16:19:30 +0530

    Update and rename basicpython.py to 02 basicpython.py

--- 02 basicpython.py
+++ 02 basicpython.py
@@ -21,4 +21,4 @@ print (A*B)      # 10 * 20 = 200 multiplication
 print (A/B)      # 10 / 20 = 0.5 division
 print (A//B)     # 10 // 20 = 0 because it used to ignore the decimal. it will give absolute value.
 print (A**B)     # 10 ** 20  = 10000000000 .it is used to get power of value.10 to the power of 20 (10^20) is a very large number, equal to 1 followed by 20 zeros. It can be written as 100,000,000,000,000,000,000. 
-print (A % B)    # 10 % 20 
+print (A % B)    # 10 % 20  ## testing
